d91c0d5288ed8692f53956ebcc0bdd9e2a2bb891
Alex MacArthur <alex.macarthur@ramseysolutions.com>
2021-10-29 13:57:40 -0500

    edit readme stuff

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-export const taxationIs = () => {
-    return "good?";
-}
+const fs = require('fs');
+
+const contents = fs.readFileSync(`${__dirname}/README.md`, 'utf8');
+const currentNumber = Number(contents);
+const newNumber = currentNumber + 1;
+
+fs.writeFileSync("README.md", String(newNumber));
